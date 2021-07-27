1caab1284faafff47a1e36085d8a51610a503f88
hongseckkim <gusduswk610@gmail.com>
2021-07-28 00:48:15 +0900

    update

---
+++ project-1/src/app.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './app.css';
+import Habit from "./components/habit";
+
+function App() {
+  const name = "hong";
+  const names = "오호라";
+  return (
+      <>
+        <Habit></Habit>
+      </>
+  );
+}
+
+export default App;
---
+++ project-1/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '@fortawesome/fontawesome-free/js/all.min.js'
+import './index.css';
+import App from './app';
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
